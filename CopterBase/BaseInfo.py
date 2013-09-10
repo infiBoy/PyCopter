ca1f3fb4ec3843aac17587d9cc3c70191cf0089d
guy B <guy@guy-Ubuntu>
2013-09-10 20:45:35 +0300

    basic properties of the copters

--- CopterBase/BaseInfo.py
+++ CopterBase/BaseInfo.py
@@ -1,11 +1,20 @@
-#This is an abstract class using abc
+#Todo: use abstract class using abc
 #for more info at abc , goto : http://docs.python.org/2/library/abc.html
-def class CopterBase(object):
-  
+class BaseInfo(object):  
   #constructor for basic properties
   def __init__(self):
     self.name = None
-    self.Id = None
+    self.id = None
+    self.type = None #the type of the drone
+    
+    #dimension of the drone
+    self.height = None
+    self.width  = None
+    self.depth  = None
+    
+    self.connectivityType = None # i.e : Wifi, 5GHZ and etc..
     
   
   
+    def Move(): 
+        pass
