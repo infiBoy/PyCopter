73068ca52a77948a612bc975ef84a0599031d2d6
infiBoy <infiBoy@users.noreply.github.com>
2013-09-10 18:51:26 +0200

    Update BaseInfo.py
    temp save

---
+++ CopterBase/BaseInfo.py
@@ -0,0 +1,11 @@
+#This is an abstract class using abc
+#for more info at abc , goto : http://docs.python.org/2/library/abc.html
+def class CopterBase(object):
+  
+  #constructor for basic properties
+  def __init__(self):
+    self.name = None
+    self.Id = None
+    
+  
+  
