9ced603c17c120298d62e0a8b6cf525fc77c8460
aravmaxim <aravmaxim@gmail.com>
2013-11-04 19:10:21 +0200

    Added all the crazyflie lib

--- PyCopterAPI/CrazyFlie/CrazyCopNet.py
+++ PyCopterAPI/CrazyFlie/CrazyCopNet.py
@@ -1,10 +1,14 @@
+from json import scanner
+from PyCopterAPI.CrazyFlie.cflib.crtp import init_drivers, scan_interfaces
 from PyCopterAPI.ICopter.ICopNet import ICopNet
+from PyCopterAPI.CrazyFlie.cflib.crazyflie import  Crazyflie
 
 class CrazyCopNet(ICopNet):
     def __init__(self):
         super(CrazyCopNet, self).__init__()
         pass
     def connect(self):
+
         pass
     def disconnect(self):
         pass
