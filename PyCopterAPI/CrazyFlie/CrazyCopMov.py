91b85f5c6816dc5f824dcc5bc29f9e1aea9ec679
NirLev <nir.levanon.a@gmail.com>
2013-11-28 11:20:55 +0100

    Added @abstractmethod for ICopMov

--- PyCopterAPI/CrazyFlie/CrazyCopMov.py
+++ PyCopterAPI/CrazyFlie/CrazyCopMov.py
@@ -1,6 +1,39 @@
 from PyCopterAPI.ICopter.ICopMov import ICopMov
 
 class CrazyCopMov(ICopMov):
+    def MoveDown(self):
+        super(CrazyCopMov, self).MoveDown()
+
+    def MoveRight(self):
+        super(CrazyCopMov, self).MoveRight()
+
+    def TakeOff(self):
+        super(CrazyCopMov, self).TakeOff()
+
+    def MoveUp(self):
+        super(CrazyCopMov, self).MoveUp()
+
+    def TurnLeft(self):
+        super(CrazyCopMov, self).TurnLeft()
+
+    def MoveLeft(self):
+        super(CrazyCopMov, self).MoveLeft()
+
+    def MoveForward(self):
+        super(CrazyCopMov, self).MoveForward()
+
+    def CommandHover(self):
+        super(CrazyCopMov, self).CommandHover()
+
+    def Land(self):
+        super(CrazyCopMov, self).Land()
+
+    def TurnRight(self):
+        super(CrazyCopMov, self).TurnRight()
+
+    def MoveBackword(self):
+        super(CrazyCopMov, self).MoveBackword()
+
     def __init__(self):
         super(CrazyCopMov, self).__init__()
         pass
--- PyCopterAPI/ICopter/ICopMov.py
+++ PyCopterAPI/ICopter/ICopMov.py
@@ -6,27 +6,39 @@ from abc import ABCMeta, abstractmethod
 
 class ICopMov:
     __metaclass__ = ABCMeta
+    @abstractmethod
     def __init__(self):
         pass
+    @abstractmethod
     def TakeOff(self):
         pass
+    @abstractmethod
     def Land(self):
         pass
+    @abstractmethod
     def CommandHover(self):
         pass
+    @abstractmethod
     def MoveLeft(self):
         pass
+    @abstractmethod
     def MoveRight(self):
         pass
+    @abstractmethod
     def MoveUp(self):
         pass
+    @abstractmethod
     def MoveDown(self):
         pass
+    @abstractmethod
     def MoveForward(self):
         pass
+    @abstractmethod
     def MoveBackword(self):
         pass
+    @abstractmethod
     def TurnLeft(self):
         pass
+    @abstractmethod
     def TurnRight(self):
         pass
